91a562b90e586d0a86b73887239bd1c7cc2bbdf8
Daniel Liuzzi <dliuzzi@gmail.com>
2021-12-26 13:29:25 +0100

    Add buckets

--- test.ps1
+++ test.ps1
@@ -1,2 +1,15 @@
-Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+function Install-Scoop {
+    # https://scoop.sh/
+    Set-ExecutionPolicy RemoteSigned -scope CurrentUser
+    Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
+
+    # Git is required for buckets. Run 'scoop install git' and try again.
+    scoop install git
+
+    # Add scoop buckets
+    scoop bucket add extras
+    scoop bucket add nerd-fonts
+    scoop bucket add nonportable
+}
+
+Install-Scoop
